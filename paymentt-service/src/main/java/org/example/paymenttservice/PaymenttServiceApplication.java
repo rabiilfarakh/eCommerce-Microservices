f55041044fc15d98876e96e1c8206e1b3a8c0517
rabiilfarakh <rabiilfarakh2816@gmail.com>
2025-02-18 23:14:02 +0100

    initial project

---
+++ paymentt-service/src/main/java/org/example/paymenttservice/PaymenttServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.paymenttservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymenttServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymenttServiceApplication.class, args);
+	}
+
+}
---
+++ paymentt-service/src/test/java/org/example/paymenttservice/PaymenttServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.paymenttservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymenttServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
