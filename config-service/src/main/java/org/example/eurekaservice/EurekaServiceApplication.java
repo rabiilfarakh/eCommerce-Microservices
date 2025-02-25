6e53b60d3c8875993acc72a918ecdd4126c10387
Douaa <douaachemnane@gmail.com>
2025-02-25 09:31:38 +0000

    commit before switch to main branch

--- config-service/src/main/java/org/example/eurekaservice/EurekaServiceApplication.java
+++ config-service/src/main/java/org/example/eurekaservice/EurekaServiceApplication.java
@@ -1,13 +1,15 @@
-package org.example.configservice;
+package org.example.eurekaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class ConfigServiceApplication {
+@EnableEurekaServer
+public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServiceApplication.class, args);
+		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
 
 }
--- config-service/src/test/java/org/example/eurekaservice/ConfigServiceApplicationTests.java
+++ config-service/src/test/java/org/example/eurekaservice/ConfigServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package org.example.configservice;
+package org.example.eurekaservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
